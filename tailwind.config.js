1aeb3a0c3342611dff6481d8d80032ecfae79ab9
Seraphin Sk <seraphinsk03@gmail.com>
2024-05-29 16:07:29 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0ea5e9'
+      },
+      fontSize: {
+        pro: '10rem'
+      }
+    },
+  },
+  plugins: [],
+}
+
